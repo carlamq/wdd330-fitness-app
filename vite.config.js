3c68f861954f7b556ec4e4aa1175f92325361481
CarlaMQ <carlaquintanar1907@gmail.com>
2025-10-16 11:46:46 +0900

    routes fixed

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
-  base: "/wdd330-fitness-app/", //GitHub Pages
-  
+  base: "/", // Para Netlify
+
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        profile: resolve(__dirname, "src/pages/profile/index.html"),
-        recipes: resolve(__dirname, "src/pages/recipes/index.html"),
-        workouts: resolve(__dirname, "src/pages/workouts/index.html"),
-        mealplan: resolve(__dirname, "src/pages/meal-plan/index.html"),
-        favorites: resolve(__dirname, "src/pages/favorites/index.html"),
+        profile: resolve(__dirname, "src/pages/profile.html"),
+        recipes: resolve(__dirname, "src/pages/recipes.html"),
+        workouts: resolve(__dirname, "src/pages/workouts.html"),
+        favorites: resolve(__dirname, "src/pages/favorites.html"),
       },
     },
   },
