dc3518b40c63265ade8dde6a37f385f85bfbea29
CarlaMQ <carlaquintanar1907@gmail.com>
2025-10-16 11:34:13 +0900

    vite pase

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
-
+  base: "/wdd330-fitness-app/", //GitHub Pages
+  
   build: {
     outDir: "../dist",
     rollupOptions: {
